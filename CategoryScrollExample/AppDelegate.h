8b1be2e7c82accb02462d512f904eead7d93668c
村上優孝 <murakami@TgMacPro04-no-MacBook-Pro.local>
2015-02-12 16:31:31 +0900

    Initial Commit

---
+++ CategoryScrollExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CategoryScrollExample
+//
+//  Created by 村上優孝 on 2015/02/12.
+//  Copyright (c) 2015年 村上優孝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CategoryScrollExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CategoryScrollExample
+//
+//  Created by 村上優孝 on 2015/02/12.
+//  Copyright (c) 2015年 村上優孝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
