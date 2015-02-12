ce4942563381d6d2d26f12eace7d2daab13e014b
村上優孝 <murakami@TgMacPro04-no-MacBook-Pro.local>
2015-02-12 18:37:31 +0900

    コメントを追加。

--- CategoryScrollExample/ViewController.m
+++ CategoryScrollExample/ViewController.m
@@ -25,8 +25,9 @@
     
     self.edgesForExtendedLayout = UIRectEdgeNone;
 
-    [self createArticleScrollView];
-    [self createCategoryScrollView];
+    [self createArticleScrollView]; // テーブルのスクロールビューを作成
+    
+    [self createCategoryScrollView]; // カテゴリーのスクロールビューを作成
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,22 +41,23 @@
     CGFloat CATEGORY_WIDTH = 120;
     CGFloat CATEGORY_HEIGHT = CATEGORY_BAR_HEIGHT;
     
+    // カテゴリーのスクロールビューを作成
     self.categoryScrollView = [[UIScrollView alloc] init];
     self.categoryScrollView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 0);
     self.categoryScrollView.backgroundColor = [UIColor lightGrayColor];
     self.categoryScrollView.contentSize = CGSizeMake(CATEGORY_WIDTH * CATEGORIES.count, CATEGORY_HEIGHT);
     self.categoryScrollView.pagingEnabled = YES;
     self.categoryScrollView.clipsToBounds = NO;
-    self.categoryScrollView.bounds = CGRectMake(0.0f, 0.0f, CATEGORY_WIDTH, 0);
+    self.categoryScrollView.bounds = CGRectMake(0.0f, 0.0f, CATEGORY_WIDTH, 0); // カテゴリーの幅をスクロールする幅にする
     self.categoryScrollView.delegate = self;
     [self.view addSubview:self.categoryScrollView];
     
-    // 線を引く
+    // カテゴリーの強調線を追加
     UIView *horizontal = [[UIView alloc] initWithFrame:CGRectMake(CATEGORY_WIDTH -30 , CATEGORY_HEIGHT, CATEGORY_WIDTH, 2)];
     horizontal.backgroundColor = [UIColor redColor];
     [self.view addSubview:horizontal];
     
-    // テーブルをカテゴリの数だけ追加
+    // カテゴリーをスクロールビューに追加
     for (int i=0; i < CATEGORIES.count; i++) {
         UILabel *label = [[UILabel alloc] init];
         label.frame = CGRectMake(CATEGORY_WIDTH * i, 0, CATEGORY_WIDTH, CATEGORY_HEIGHT);
@@ -69,6 +71,7 @@
     CGFloat TABLE_HEIGHT = self.view.bounds.size.height;
     CGRect tableBounds = CGRectMake(0.0f, CATEGORY_BAR_HEIGHT, TABLE_WIDTH, TABLE_HEIGHT - [UIApplication sharedApplication].statusBarFrame.size.height - self.navigationController.navigationBar.frame.size.height - CATEGORY_BAR_HEIGHT);
     
+    // テーブルのスクロールビューを作成
     self.tableScrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
     self.tableScrollView.contentSize = CGSizeMake(TABLE_WIDTH * CATEGORIES.count, TABLE_HEIGHT);
     self.tableScrollView.pagingEnabled = YES;
@@ -77,7 +80,7 @@
     [self.view addSubview:self.tableScrollView];
     
     
-    // UITableView をUIScrollView に追加
+    // テーブルをスクロールビューに追加
     CGRect tableFrame = tableBounds;
     tableFrame.origin.x = 0.0f;
     for (int i = 0; i < CATEGORIES.count; i++) {
@@ -109,7 +112,7 @@
     CGFloat pageWidth = scrollView.frame.size.width;
     float fractionalPage = scrollView.contentOffset.x / pageWidth;
     if (scrollView == self.tableScrollView) {
-        [self.categoryScrollView setContentOffset:CGPointMake(self.categoryScrollView.frame.size.width * fractionalPage, self.categoryScrollView.frame.origin.y) animated:NO];
+        [self.categoryScrollView setContentOffset:CGPointMake(self.categoryScrollView.frame.size.width * fractionalPage, self.categoryScrollView.frame.origin.y) animated:NO]; // テーブルのスクロールビューが移動した割合だけカテゴリーのスクロールビューも移動する
     }
 }
 
