d406d677d2d88ef91f04a6aafd42b0b9e0ec4d32
村上優孝 <murakami@TgMacPro04-no-MacBook-Pro.local>
2015-02-12 18:11:11 +0900

    スクリーンショットを追加

--- CategoryScrollExample/ViewController.m
+++ CategoryScrollExample/ViewController.m
@@ -23,8 +23,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.title = CATEGORIES.firstObject;
-    
     self.edgesForExtendedLayout = UIRectEdgeNone;
 
     [self createArticleScrollView];
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-README
+スクリーンショット
+
+![screenshot](screenshot.gif)
